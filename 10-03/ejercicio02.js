c20d35d30eaf3747caaafde8ddc7ec7a53058c17
juanusuga033 <juanusugam@ielacandelariamedellin.edu.co>
2026-03-10 21:01:33 +0000

    ejercicios

---
+++ 10-03/ejercicio02.js
@@ -0,0 +1,17 @@
+let num1=3;
+console.log(num1)
+
+let num2=33;
+console.log(num2)
+
+let suma = num1 + num2;
+console.log(suma)
+
+let resta = num1 - num2;
+console.log(resta)
+
+let multiplicacion = num1 * num2;
+console.log(multiplicacion)
+
+let division = num1 / num2;
+console.log(division)
