9e4abfbdfe8c7ad09b517f4185b050f678e10364
juanusuga033 <juanusugam@ielacandelariamedellin.edu.co>
2026-04-21 21:10:55 +0000

    mensaje

---
+++ 21-04/ejercicio5-10.js
@@ -0,0 +1,9 @@
+
+
+let suma = 0;
+
+for (let i = 1; i <= 100; i++) {
+  suma += i;
+}
+
+console.log("La suma de todos los números del 1 al 100 es: " + suma);
---
+++ 21-04/ejercicio5-6.js
@@ -0,0 +1,6 @@
+
+const numero = 3;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${numero} x ${i} = ${numero * i}`);
+}
